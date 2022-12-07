abd0370e26705fca6fecee969cba9890f8cef223
JIHYEISWISDOM <wlgp8958@naver.com>
2022-12-07 23:35:59 +0900

    feat: Car 객체에 필요한 상수처리와 생성자

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,10 +1,21 @@
 package racingcar;
 
 public class Car {
+
+    private static final String ENTER_LINE = "\n";
+    private static final String WHITE_SPACE = " ";
+    private static final String NO_STRING = "";
+    private static final String COLON = " : ";
+    private static final String DASH = "-";
+    private static final int MAX_LENGTH = 5;
+    private static final int EMPTY = 0;
+
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateEmpty(name);
+        validateLength(name);
         this.name = name;
     }
 
