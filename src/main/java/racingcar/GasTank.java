f55102e38ab462b202c3c902dc38977a2346cba3
JIHYEISWISDOM <wlgp8958@naver.com>
2022-12-07 23:44:36 +0900

    feat: GasTank에 필요한 상수와 생성자 만들기

---
+++ src/main/java/racingcar/GasTank.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class GasTank {
+
+    private static final int MIN_GAS = 0;
+    private static final int MAX_GAS = 9;
+    private static final int ENOUGH_GAS = 4;
+
+    private GasTank() {
+    }
+
+}
