ab27df7e55ee9535e81d4ac800ab5a28a3f612ba
JIHYEISWISDOM <wlgp8958@naver.com>
2022-12-07 23:37:58 +0900

    feat: Car 객체 유효성 검사 메서드

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -19,5 +19,20 @@ public class Car {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public static Car newCar(String name) {
+        return new Car(name);
+    }
+
+    private void validateEmpty(String name) {
+        if (name.length() == EMPTY || name.replaceAll(WHITE_SPACE, NO_STRING).length() == EMPTY) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+    }
+
+    private void validateLength(String name) {
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("각 자동차의 이름은 5자 이하여야 합니다.");
+        }
+    }
+
 }
