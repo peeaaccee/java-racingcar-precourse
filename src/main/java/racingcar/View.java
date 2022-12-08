c696769d0f7c655667008521989644fd7c658e7d
JIHYEISWISDOM <wlgp8958@naver.com>
2022-12-08 20:18:50 +0900

    feat: View에 필요한 상수, 생성자 만들기

---
+++ src/main/java/racingcar/View.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class View {
+    private static final String PLEASE_INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String PLEASE_INPUT_LOOP_TIME = "시도할 회수는 몇회인가요?";
+    private static final String ERROR = "[ERROR] ";
+    private static final String WINNER = "최종 우승자: ";
+    private static final String JOIN_COMMA = ", ";
+    private static final String START_RACING = "\n실행 결과";
+
+    private View() {
+    }
+
+}
